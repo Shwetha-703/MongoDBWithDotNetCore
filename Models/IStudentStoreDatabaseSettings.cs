07e06645bbfc3fcd57b2f4caa8f08b81b2c04cef
prashant-humney <prashant.humney89@gmail.com>
2023-02-02 15:54:04 +0530

    Adding to StudentManagement

---
+++ Models/IStudentStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StudentManagement.Models
+{
+	public interface IStudentStoreDatabaseSettings
+	{
+		public string StudentCoursesCollectionName { get; set; }
+		public string ConnectionString { get; set; }
+		public string DatabaseName { get; set; }
+	}
+}
+
